d81b5affb3ba5147c820de7829625a9aa7025f9d
Alessio Ottaviani <alessiottaviani@protonmail.com>
2024-07-05 10:30:01 +0200

    greet service setup

---
+++ greet/server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/alessiottavo/grpc-go/greet/proto"
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+var addr string = "0.0.0.0:50051"
+
+func main() {
+
+	var lis, err = net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatalf("Failed to Listen on: %v\n", err)
+	}
+
+	log.Printf("Listening on %s\n", addr)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v\n", err)
+	}
+}
